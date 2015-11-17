e0eb38998d53c713e8f9e486b263ffb5f8fbcfe9
Fabian Kessler <fabian.kessler@optimaize.com>
2015-11-17 22:10:01 +0200

    Moved code from nameapi-client-php project to here.

---
+++ tests/functional/org/nameapi/client/services/BaseServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace org\nameapi\client\services;
+
+//require '../../../../../../vendor/autoload.php';
+
+use org\nameapi\ontology\input\context\Context;
+use org\nameapi\ontology\input\context\Priority;
+use org\nameapi\ontology\input\context\TextCase;
+
+
+abstract class BaseServiceTest extends \PHPUnit_Framework_TestCase {
+
+    private $apiKey = null;
+
+    /**
+     * @return Context
+     */
+    protected function makeContext() {
+        if (!$this->apiKey) {
+            die("Put your api key in the \$apiKey variable to run these functional tests!");
+        }
+        return Context::builder()
+            ->priority(Priority::REALTIME())
+            ->textCase(TextCase::TITLE_CASE())
+            ->build();
+    }
+
+    /**
+     * @return ServiceFactory
+     */
+    protected function makeServiceFactory() {
+        //currently connecting to the preview server because v5.0 is not live yet.
+        return new ServiceFactory($this->apiKey, $this->makeContext(), new Host('http', 'preview-api.nameapi.org', 80), '5.0');
+    }
+
+}
